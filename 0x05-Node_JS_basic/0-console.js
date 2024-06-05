18d0635f597c5039daa1b16a9446f5a3ed83941e
IK-18 <ik.arigbe.osula@gmail.com>
2024-06-05 08:19:22 +0000

    all tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Osaite Arigbe-Osula <https://github.com/IK-18>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
